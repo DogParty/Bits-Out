e88620d03810c7c7ac6184771a975dc29f157070
Smith7018 <smith7018@gmail.com>
2012-02-16 10:54:56 -0500

    Working on tiny code again.  Got it down to .89 Kb before this, so lets see..

--- src/BitsOutTinyCode.java
+++ src/BitsOutTinyCode.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 public class BitsOut {
-	public static int a[] = {35,71,142,284,536,1121,2274,4548,9096,17168,35872,72768,145536,291072,549376,1147904,2328576,4657152,9314304,17580032,3178496,7405568,14811136,29622272,25690112};
+	public static int a[]={35,71,142,284,536,1121,2274,4548,9096,17168,35872,72768,145536,291072,549376,1147904,2328576,4657152,9314304,17580032,3178496,7405568,14811136,29622272,25690112};
 	public static void main(String[] args){
 		System.out.println("Welcome!");
-		int b = 386320;
-		p(b);
+		p();
 	}
-	private static void p(int b) {
-		Scanner k = new Scanner(System.in);
+	private static void p() {
+		Scanner k=new Scanner(System.in);
+		int b=386320;
 		while (true){
 			d(b);
 			System.out.println("\nEnter button [0-24]: ");
 			int i=k.nextInt();
-			b =f(b,i);
+			b=f(b,i);
 			if (b==0) break;
 		}
 		System.out.println("!");
